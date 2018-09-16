b466f129fed7c6375dad2875baede71f3339b1e3
Henrique Dos Santos <henrique@henriques-macbook-pro.local>
2018-09-15 22:08:50 -0400

    making corners rounded

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var profileLogoImg: UIImageView!
+    @IBOutlet weak var customizeDashboardBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         profileLogoImg.layer.cornerRadius = 7
+        customizeDashboardBtn.layer.cornerRadius = 7
     }
 
 
